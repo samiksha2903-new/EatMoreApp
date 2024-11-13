7a7d1de50b64d9a9c99c9439749459f6e5a1002a
Samiksha Chaudhary <samiksha.choudhary2003@gmail.com>
2024-11-13 17:16:49 +0530

    make a change in position of chef image

--- src/components/images/Chef.jsx
+++ src/components/images/Chef.jsx
@@ -4,7 +4,7 @@ import chefImage from './chef.png'
 const Chef = () => {
   return (
     <div className='mx-6'>
-        <img className='h-65 relative animate-float' src={chefImage} alt="img" />
+        <img className='h-65 sticky top-40 animate-float' src={chefImage} alt="img" />
     </div>
   )
 }
